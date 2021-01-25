ea5cfdacc35112d74d6eaa4e12e974ac283c8963
Chris Flores <40873769+flores58c@users.noreply.github.com>
2021-01-24 18:48:54 -0800

    Still need stuff look on discord

--- project1.sql
+++ project1.sql
@@ -66,7 +66,9 @@ last_filled date,
 quantity int(100),
 pharmacy_id char(10),
 foreign key(doc_id) references doctor(doc_id),
-foreign key (pharmacy_id) references pharmacy(pharmacy_id)
+foreign key (pharmacy_id) references pharmacy(pharmacy_id),
+foreign key(patient_id) references patient(patient_id),
+foreign key(drug_id) references drug(drug_id)
 );
 -- pharmacy id for prescripton
 
@@ -86,7 +88,8 @@ insert into prescription values
 create table pharmComp
 (comp_id char(5),
 compName char(30),
-company_phone char(20)
+company_phone char(20),
+primary key(comp_id)
 );
 
 insert into pharmComp values
@@ -103,19 +106,20 @@ create table drug
 (drug_id int(10),
 trade_name char(50),
 formula char(50),
+drug_price float(2),
 primary key (drug_id));
 
 insert into drug values 
-(01,'Parlodel','C32H40BrN5O5'), -- oncology
-(02,'Dostinex','C26H37N5O2'),
-(03,'Comirnaty','BNT162b2'), -- covid
-(04,'Advil','C13H18O2'), -- headache
-(05,'Sectral','C18H28N2O4'), -- cardiology
-(06,'Tenormin','C14H22N2O3'),
-(07,'Zyrtec','C21H25ClN2O32HCl'), -- allergies
-(08,'Allegra','C32H39NO4'), -- allergies
-(09,'Ibuprofen','C13H18O2'), -- children 
-(10,'Amoxicillin','C16H19N3O5S'); -- antibiotic
+(01,'Parlodel','C32H40BrN5O5',1.11), -- oncology
+(02,'Dostinex','C26H37N5O2',2.22),
+(03,'Comirnaty','BNT162b2',3.33), -- covid
+(04,'Advil','C13H18O2',4.44), -- headache
+(05,'Sectral','C18H28N2O4',5.55), -- cardiology
+(06,'Tenormin','C14H22N2O3',6.66),
+(07,'Zyrtec','C21H25ClN2O32HCl',7.77), -- allergies
+(08,'Allegra','C32H39NO4',8.88), -- allergies
+(09,'Ibuprofen','C13H18O2',9.99), -- children 
+(10,'Amoxicillin','C16H19N3O5S',10.10); -- antibiotic
 
 -- we decided to normalize supervisor table to keep the supevisors for each company separate
 -- pharamcies don't need the other supervisors from other stores
@@ -123,34 +127,49 @@ insert into drug values
 create table supervisor_RIT
 (
 s_id int(255),
-s_name char(15) not null -- supervisor can change and there needs to be a supervisor 
-
+s_name char(15) not null, -- supervisor can change and there needs to be a supervisor 
+primary key(s_id)
 );
+
+insert into supervisor_RIT values
+(1,"Bob"),
+(2,'Bill'),
+(3,'Roger')
+;
 
 create table supervisor_WAL
 (
 s_id int(255),
-s_name char(15) not null -- supervisor can change and there needs to be a supervisor 
+s_name char(15) not null,-- supervisor can change and there needs to be a supervisor 
+primary key(s_id)
 );
+
+insert into supervisor_WAL values
+
+(1,"Henry"),
+(2,'Karen'),
+(3,'Keith')
+;
 
 create table supervisor_CVS
 (
 s_id int(255),
-s_name char(15) not null -- supervisor can change and there needs to be a supervisor 
+s_name char(15) not null, -- supervisor can change and there needs to be a supervisor 
+primary key (s_id)
 );
 
 
-insert into supervisor values
+insert into supervisor_CVS values
 
-(1,"Henry"),
-(2,'Karen'),
+(1,"Hank"),
+(2,'Kathy'),
 (3,'Jacob')
 ;
 
 
 -- not simplified because the contract needs all these items
 -- text of contract price of drug and agreement on payment to company
-create table contract
+create table contract_CVS
 (contract_id char(100),
 start_date date,
 end_date date,
@@ -158,15 +177,65 @@ s_id int(255),
 comp_id char(10),
 pharmacy_id char(10),
 drug_id int(255),
+markup float(2),
 con_text char(100),
-foreign key (drug_id) references drugs(drug_id),
-foreign key (s_id) references supervisor(s_id),
+primary key (contract_id),
+foreign key (drug_id) references drug(drug_id),
+foreign key (s_id) references supervisor_CVS(s_id),
 foreign key(comp_id) references pharmComp(comp_id),
 foreign key(pharmacy_id) references pharmacy(pharmacy_id)
 );
 
+create table contract_RIT
+(contract_id char(100),
+start_date date,
+end_date date,
+s_id int(255),
+comp_id char(10),
+pharmacy_id char(10),
+drug_id int(255),
+markup float(2),
+con_text char(100),
+primary key (contract_id),
+foreign key (drug_id) references drug(drug_id),
+foreign key (s_id) references supervisor_RIT(s_id),
+foreign key(comp_id) references pharmComp(comp_id),
+foreign key(pharmacy_id) references pharmacy(pharmacy_id)
+);
+
+create table contract_WAL
+(contract_id char(100),
+start_date date,
+end_date date,
+s_id int(255),
+comp_id char(10),
+pharmacy_id char(10),
+drug_id int(255),
+markup float(2),
+con_text char(100),
+primary key (contract_id),
+foreign key (drug_id) references drug(drug_id),
+foreign key (s_id) references supervisor_WAL(s_id),
+foreign key(comp_id) references pharmComp(comp_id),
+foreign key(pharmacy_id) references pharmacy(pharmacy_id)
+);
+-- allegra -- ibuprofen -- advil -- zyrtec
 -- 2 Where is the prescription being made? Prescription -> Pharmacy -> Contract
-insert into contract values
-('1W','2021/01/10','2023/01/10',1,'JJ','WAL',01,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' )
--- 
+insert into contract_WAL values
+('1W','2021/01/10','2023/01/10',1,'JJ','WAL',01,.25,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
+('2W', '2021/01/10','2023/01/10',1,'JJ','WAL',02,.25,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
+('3W','2021/01/20','2023/01/20',2,'PFZ','WAL',03,.25,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25'),
+('4W','2020/09/09','2021/09/09',1,'NV','WAL',08,.25,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25')
+;
+
+insert into contract_RIT values
+('1R','2021/01/01','2023/01/01',1,'PFZ','RIT',04,.15,'Rite-Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
+('2R','2020/01/01','2021/01/01',2,'MD','RIT',05,.15,'Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
+('3R','2020/03/01','2021/03/01',3,'MD','RIT',06,.15,'Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15')
+;
+
+insert into contract_CVS values
+('1C','2020/02/02','2021/02/02',1,'NV','CVS',07,.2,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('2C','2010/01/01','2025/01/01',2,'MK','CVS',09,.2,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('3C','2020/01/01','2021/01/01',3,'MK','CVS',10,.2,'CVS Pharmacy agrees to pay for drug @ marginal cost and plans to sell at Markup .20')
 ;
