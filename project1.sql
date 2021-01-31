c51fa9b9397abf6690e2b4272d5bde43ce81a625
Chris Flores <40873769+flores58c@users.noreply.github.com>
2021-01-30 19:27:06 -0800

    Finished database

--- project1.sql
+++ project1.sql
@@ -180,8 +180,10 @@ end_date date,
 s_id int(255),
 comp_id char(10),
 drug_id int(255),
+pharmacy_id char(10),
 con_text char(100),
 primary key (contract_id),
+foreign key (pharmacy_id) references pharmacy(pharmacy_id),
 foreign key (drug_id) references drug(drug_id),
 foreign key (s_id) references supervisor_CVS(s_id),
 foreign key(comp_id) references pharmComp(comp_id)
@@ -194,8 +196,10 @@ end_date date,
 s_id int(255),
 comp_id char(10),
 drug_id int(255),
+pharmacy_id char(10),
 con_text char(100),
 primary key (contract_id),
+foreign key (pharmacy_id) references pharmacy(pharmacy_id),
 foreign key (drug_id) references drug(drug_id),
 foreign key (s_id) references supervisor_RIT(s_id),
 foreign key(comp_id) references pharmComp(comp_id)
@@ -209,8 +213,10 @@ end_date date,
 s_id int(255),
 comp_id char(10),
 drug_id int(255),
+pharmacy_id char(10),
 con_text char(100),
 primary key (contract_id),
+foreign key (pharmacy_id) references pharmacy(pharmacy_id),
 foreign key (drug_id) references drug(drug_id),
 foreign key (s_id) references supervisor_WAL(s_id),
 foreign key(comp_id) references pharmComp(comp_id)
@@ -218,26 +224,26 @@ foreign key(comp_id) references pharmComp(comp_id)
 -- allegra -- ibuprofen -- advil -- zyrtec
 -- 2 Where is the prescription being made? Prescription -> Pharmacy -> Contract
 insert into contract_WAL values
-('1W','2021/01/10','2023/01/10',1,'JJ',01,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
-('2W', '2021/01/10','2023/01/10',1,'JJ',02,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
-('3W','2021/01/20','2023/01/20',2,'PFZ',03,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25'),
-('4W','2020/09/09','2021/09/09',1,'NV',08,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25'),
-('5W','2021/03/01','2023/03/01',3,'PFZ',04,'Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25')
+('1W','2021/01/10','2023/01/10',1,'JJ',01,'WAL','Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
+('2W', '2021/01/10','2023/01/10',1,'JJ',02,'WAL','Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25' ),
+('3W','2021/01/20','2023/01/20',2,'PFZ',03,'WAL','Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25'),
+('4W','2020/09/09','2021/09/09',1,'NV',08,'WAL','Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25'),
+('5W','2021/03/01','2023/03/01',3,'PFZ',04,'WAL','Walgreens agrees to pay for drug @ marginal cost and plans to sell at Markup .25')
 ;
 
 insert into contract_RIT values
-('1R','2021/01/01','2023/01/01',1,'PFZ',04,'Rite-Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
-('2R','2020/01/01','2021/01/01',2,'MD',05,'Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
-('3R','2020/03/01','2021/03/01',3,'MD',06,'Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
-('4R','2020/07/09','2021/07/09',1,'NV',08,'Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15')
+('1R','2021/01/01','2023/01/01',1,'PFZ',04,'RIT','Rite-Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
+('2R','2020/01/01','2021/01/01',2,'MD',05,'RIT','Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
+('3R','2020/03/01','2021/03/01',3,'MD',06,'RIT','Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15'),
+('4R','2020/07/09','2021/07/09',1,'NV',08,'RIT','Rite Aid agrees to pay for drug @ marginal cost and plans to sell at Markup .15')
 ;
 
 insert into contract_CVS values
-('1C','2020/02/02','2021/02/02',1,'NV',07,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
-('2C','2010/01/01','2025/01/01',2,'MK',09,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
-('3C','2020/01/01','2021/01/01',3,'MK',10,'CVS Pharmacy agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
-('4C','2021/04/01','2023/04/01',1,'PFZ',04,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
-('5C','2020/02/09','2021/02/09',1,'NV',08,'CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20')
+('1C','2020/02/02','2021/02/02',1,'NV',07,'CVS','CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('2C','2010/01/01','2025/01/01',2,'MK',09,'CVS','CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('3C','2020/01/01','2021/01/01',3,'MK',10,'CVS','CVS Pharmacy agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('4C','2021/04/01','2023/04/01',1,'PFZ',04,'CVS','CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20'),
+('5C','2020/02/09','2021/02/09',1,'NV',08,'CVS','CVS agrees to pay for drug @ marginal cost and plans to sell at Markup .20')
 ;
 
 create table markup_WAL
